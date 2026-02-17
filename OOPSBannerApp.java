49c1c589bd932943d929fc725e860e4f2e5128d4
Niharika <nv0445@srmist.edu.in>
2026-02-17 15:09:00 +0530

    Updated Use Case 2 with banner print statements

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,3 +1,11 @@
-public static void main(String[] args) {
-   System.out.println("OOPS");
+public class OOPSBannerApp {
+   public static void main(String[] args)
+{
+     System.out.println(" 00000  00000  PPPPP  SSSSS ");
+     System.out.println(" 0   00   0   P   P S       ");
+     System.out.println(" 0   00   0    ppppp  SSSSS ");
+     System.out.println(" 0   00   0    P          S ");
+     System.out.println(" 00000  00000  P      SSSSS ");
+     }
 }
+  
