7099e4c7951e39e0344dbc203b5f8ae3ce36d499
Niharika <nv0445@srmist.edu.in>
2026-02-21 13:46:05 +0530

    Use Case 4: Banner using Array and Loop

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,10 +1,19 @@
 public class OOPSBannerApp {
-    public static void main(String[] args)
-{
-    String part1 = "OO";
-    String part2 = "PS";
-   
-    String result = String.join("", part1, part2);
-    System.out.println(result);
-   }
+
+    public static void main(String[] args) {
+
+        // Store banner lines in an array
+        String[] banner = {
+                " OOOOO   OOOOO   PPPPP   SSSSS ",
+                "O     O O     O  P    P S      ",
+                "O     O O     O  PPPPP   SSSSS ",
+                "O     O O     O  P            S",
+                " OOOOO   OOOOO   P       SSSSS "
+        };
+
+        // Print using loop
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
 }
