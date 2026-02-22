e45fc182be7ba3d766399fb0ea5043f64369167d
NIHARIKA V <nv0445@srmist.edu.in>
2026-02-22 21:00:52 +0530

    Update OOPSBannerApp.java

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,32 +1,12 @@
-public class OOPSBannerApp {
-<<<<<<< HEAD
-   public static void main(String[] args)
-{
-     System.out.println(" 00000  00000  PPPPP  SSSSS ");
-     System.out.println(" 0   00   0   P   P S       ");
-     System.out.println(" 0   00   0    ppppp  SSSSS ");
-     System.out.println(" 0   00   0    P          S ");
-     System.out.println(" 00000  00000  P      SSSSS ");
-     }
-}
-  
-=======
+  public static void main(String[] args) {
 
-    public static void main(String[] args) {
+        System.out.println(" *****   *****   *****   ***** ");
+        System.out.println("*     * *     * *     * *     *");
+        System.out.println("*     * *     * *     * *      ");
+        System.out.println("*     * *     * *****   *****  ");
+        System.out.println("*     * *     * *              *");
+        System.out.println("*     * *     * *              *");
+        System.out.println(" *****   *****  *         ***** ");
 
-        // Store banner lines in an array
-        String[] banner = {
-                " OOOOO   OOOOO   PPPPP   SSSSS ",
-                "O     O O     O  P    P S      ",
-                "O     O O     O  PPPPP   SSSSS ",
-                "O     O O     O  P            S",
-                " OOOOO   OOOOO   P       SSSSS "
-        };
-
-        // Print using loop
-        for (String line : banner) {
-            System.out.println(line);
-        }
     }
 }
->>>>>>> 900bdedf87401d390f758878f16aa5ff834bbfa1
