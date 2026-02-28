2acd6d8377474adde3fe1476c8fee19d23b4aef1
Niharika <nv0445@srmist.edu.in>
2026-02-28 13:45:31 +0530

    Completed Usecase 5

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,26 +1,14 @@
 public class OOPSBannerApp {
-<<<<<<< HEAD
-   public static void main(String[] args)
-{
-     System.out.println(" 00000  00000  PPPPP  SSSSS ");
-     System.out.println(" 0   00   0   P   P S       ");
-     System.out.println(" 0   00   0    ppppp  SSSSS ");
-     System.out.println(" 0   00   0    P          S ");
-     System.out.println(" 00000  00000  P      SSSSS ");
-     }
-}
-  
-=======
 
     public static void main(String[] args) {
 
         // Store banner lines in an array
         String[] banner = {
-                " OOOOO   OOOOO   PPPPP   SSSSS ",
-                "O     O O     O  P    P S      ",
-                "O     O O     O  PPPPP   SSSSS ",
-                "O     O O     O  P            S",
-                " OOOOO   OOOOO   P       SSSSS "
+            " OOOOO   OOOOO   PPPPP   SSSSS ",
+            "O     O O     O  P    P S      ",
+            "O     O O     O  PPPPP   SSSSS  ",
+            "O     O O     O  P            S ",
+            " OOOOO   OOOOO   P       SSSSS  "
         };
 
         // Print using loop
@@ -29,4 +17,3 @@ public class OOPSBannerApp {
         }
     }
 }
->>>>>>> 900bdedf87401d390f758878f16aa5ff834bbfa1
